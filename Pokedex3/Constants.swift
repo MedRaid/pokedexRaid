b8330b10905ce95e20a2de43ab2e7e6d4ab98758
Mohamed Raid <mohamedraid.raddaoui@esprit.tn>
2017-06-14 16:54:36 +0100

    addding details for each poke

---
+++ Pokedex3/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Pokedex3
+//
+//  Created by Raddaoui Mohamed Raid on 6/14/17.
+//  Copyright © 2017 tn.esprit.Pokedex3. All rights reserved.
+//
+
+import Foundation
---
+++ Pokedex3/DetailsPokemonsController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailsPokemonsController.swift
+//  Pokedex3
+//
+//  Created by Raddaoui Mohamed Raid on 6/12/17.
+//  Copyright © 2017 tn.esprit.Pokedex3. All rights reserved.
+//
+
+import UIKit
+
+class DetailsPokemonsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
